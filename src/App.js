c3b025ea495ac8619b6ad9b5c0e0999c92dd32f8
haruchika-kurosawa <bobmarimotobe@gmail.com>
2022-11-16 18:49:40 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { v4 as uuid } from 'uuid';
+import {React, useState} from 'react';
+
+const data = {
+  "data": [
+    {
+      "key": uuid(),
+      "text": "ダミーダミーダミーダミーダミーダミー",
+      "done": false
+    },
+    {
+      "key": uuid(),
+      "text": "ダミーダミーダミーダミー",
+      "done": true
+    },
+    {
+      "key": uuid(),
+      "text": "ダミーダミーダミーダミーダミーダミーダミーダミーダミーダミー",
+      "done": false
+    },
+  ]
+}
 
 function App() {
+  const [tasks, setTasks] = useState(data.data);
+
   return (
     <div className="App">
+      {
+        tasks.map((task) => {
+          return (
+            <>
+              <li>
+                <p><span>key: </span><em>{task.key}</em></p>
+                <p><span>text: </span><em>{task.text}</em></p>
+                <p><span>done: </span><em>{task.done ? '完了' : '未完了'}</em></p>
+              </li>
+            </>
+          );
+        })
+      }
       <p>test</p>
     </div>
   );
